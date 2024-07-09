42035e177622c9a3746506a1b2fbd5fbe53a122c
blindmanmyname@gmail.com <blindmanmyname@gmail.com>
2024-07-09 10:35:56 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using AuthApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var user = await _context.Users
+            .SingleOrDefaultAsync(u => u.Username == request.Username && u.Password == request.Password);
+
+        if (user == null)
+            return Unauthorized(new { Message = "Invalid username or password" });
+
+        return Ok(new { Message = "Login successful", User = user });
+    }
+}
+
+public class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
